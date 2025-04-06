13782c5a37bbb764a6b8bff7b895b4451774fe03
Berhiddich Abderrahim <berhiddich@gmail.com>
2025-04-06 16:53:41 +0100

    add list of student

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+Route::resource('/students', StudentController::class);
